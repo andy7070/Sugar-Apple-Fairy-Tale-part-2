3d362699b16147af5a7b25d3d59630a384835d06
Anderson Felipe <136264571+andy7070@users.noreply.github.com>
2023-08-22 06:21:16 -0300

    update

--- js.js
+++ js.js
@@ -1,57 +1,56 @@
 function clickMenu(){
-    if(animes.style.display == 'none'){
-      animes.style.display = 'block'
-    } else {
-      animes.style.display = 'none'
-    }
-     animes.style.transition = ' 1s'
-    }
-    
-    function clickMenu2(){
-    if(animes2.style.display == 'none'){
-    animes2.style.display = 'block'}else{
-    animes2.style.display = 'none'}}
-    
-    function clickMenu3(){
-    if(animes3.style.display == 'none'){
-        animes3.style.display = 'block'
-    } else{
-    animes3.style.display = 'none'}}
-    
-    
-    function clickMenu4(){
-    if(animes4.style.display == 'none'){
-    animes4.style.display = 'block'}else{
-     animes4.style.display = ' none'}}
-    
-     function clickMenu5(){
-    if(animes5.style.display == 'none'){
-     animes5.style.display = 'block' }else{
-      animes5.style.display = 'none' }
-    }
-    
-    
-    function clickMenu6(){
-     if(animes6.style.display == 'none'){
-       animes6.style.display= 'block'}else{
-      animes6.style.display = 'none'}}
-    
+  
+  if(animes.style.display == 'block'){
+    animes.style.display = 'none'
+  } else {
+    animes.style.display = 'block'
+  }
+
+   
+  }
+  
+  function clickMenu2(){
+  if(animes2.style.display == 'block'){
+  animes2.style.display = 'none'}else{
+  animes2.style.display = 'block'}}
+  
+  function clickMenu3(){
+  if(animes3.style.display == 'block'){
+      animes3.style.display = 'none'
+  } else{
+  animes3.style.display = 'block'}}
+  
+  
+  function clickMenu4(){
+  if(animes4.style.display == 'block'){
+  animes4.style.display = 'none'}else{
+   animes4.style.display = ' block'}}
+  
+   function clickMenu5(){
+  if(animes5.style.display == 'block'){
+   animes5.style.display = 'none' }else{
+    animes5.style.display = 'block' }
+  }
+  
+  
+  function clickMenu6(){
+   if(animes6.style.display == 'block'){
+     animes6.style.display= 'none'}else{
+    animes6.style.display = 'block'}}
+  
 
 
-    function setaEp(){
-      if(epep.style.display == 'none'){
-      epep.style.display = 'block'} else{
-      epep.style.display = 'none'}
+  function setaEp(){
+    if(epep.style.display == 'block'){
+    epep.style.display = 'none'} else{
+    epep.style.display = 'block'}
 
-      epep.style.transition = '0.7s'
+    epep.style.transition = '0.7s'
 
-      if(epseta.style.transform == 'rotateX(0deg)'){
-      epseta.style.transform = 'rotateX(180deg)'}else{
-      epseta.style.transform = 'rotateX(0deg)'}
+    if(epseta.style.transform == 'rotateX(0deg)'){
+    epseta.style.transform = 'rotateX(180deg)'}else{
+    epseta.style.transform = 'rotateX(0deg)'}
 
-      
-    }
-
-
-
+    
+  }
     
